fccf0f7e6aa9f3dab1ea88a91a16583b806f1503
ngadiono <cristiano_ondoel@yahoo.com>
2011-12-28 00:11:55 +0700

    nambahin konten left

--- index.php
+++ index.php
@@ -156,14 +156,73 @@
             </div>
             <div class="content">
                 <div class="content_left">
-                    
+                    <style>
+                        .content_left ul
+                        {
+                            margin: 0;
+                            padding: 0;
+                        }
+                        .content_left ul #lili
+                        {
+                            list-style: none;
+                            height: 200px;
+                            width: 170px;
+                            margin-left: auto;
+                            margin-right: auto;
+                            margin-bottom: 15px;
+                            border-radius: 15px;
+                            text-align: center;
+                            padding: 5px;
+                        }
+                        .content_left ul li img
+                        {
+                            height: 50px;
+                            width: 100px;
+                            
+                        }
+                        .content_left ul li p
+                        {
+                            
+                            margin: 0;
+                            padding: 0;
+                        }
+                    </style>
+                    <ul>
+                        <li style="background-image: url('image/menu.jpg');" id="lili">
+                            <ul style="margin: 0;padding: 0;list-style: none">
+                                <li style="border-bottom: 1px solid black;height: 20px">Main Menu</li>
+                                <li style="color: #ffb3cd;height: 30px; margin-top: 20px">Home</li>
+                                <li style="color: #ffb3cd;height: 30px">My Account</li>
+                                <li style="color: #ffb3cd;height: 30px">Progress</li>
+                                <li style="color: #ffb3cd;height: 30px">Shipment</li>
+                                <li style="color: #ffb3cd;height: 30px">Confirmation of Payments</li>
+                            </ul>
+                        </li>
+                        <li style="background-image: url('image/menu.jpg');" id="lili">
+                            
+                        </li>
+                        <li style="text-align: center">
+                            <img src="image/bni.gif"/><br/><p>0201226431<br/>Siska Pratiwi</p>
+                            <img src="image/mandiri.gif"/><p>149004914372<br/>Sheli Rosita</p>
+                            <img src="image/tiki.gif"/>
+                        </li>
+                    </ul>
                 </div>
                 <div class="content_right">
                     
                 </div>
             </div>
             <div class="footer">
-                footer
+                <style>
+                    .footer p
+                    {
+                        font-size: 13px;
+                        
+                    }
+                </style>
+                <p>
+                    Luvshop Boutique Head Office & Wholesale Center<br/><br/>Jl.Wahid Hasyim No.1 Dabag Yogyakarta<br/>+628975841584<br/><br/>Copyright &COPY; 2011.All Right Reserved.
+                </p>
             </div>
         </div> <!--end wrap-->
 
