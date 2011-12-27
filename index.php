254fff7225cc114ad34807449cdd68c41f8176e5
ngadiono <cristiano_ondoel@yahoo.com>
2011-12-27 19:41:20 +0700

    nambahin gambar header

--- index.php
+++ index.php
@@ -8,45 +8,135 @@
     </head>
     <body>
         <div class="wrap"> <!--start wrap-->
-        <div class="header">
-            <style>
-                .header img
-                {
-                    height: 160px;
-                    width: 370px;
-                    position: absolute;
-                    margin-top: -20px;
-                    margin-left: -10px;
-                }
-                .header .home_img
-                {
-                    border: 1px solid black;
-                    height: 270px;
-                    width: 720px;
-                    position: absolute;
-                    margin-top: 140px;
-                    margin-left: 40px;
-                    padding: 0;
-                }
-                
-                .header .home_img #img1
-                {
-                    height: 200px;
-                    width: 200px;
-                }
-            </style>
-            <img src="image/logo.png"/>
-            <div class="home_img">
-                <img src="image/worktime.gif" id="img1"/>
+            <div class="header">
+                <style>
+                    .header img
+                    {
+                        height: 160px;
+                        width: 370px;
+                        position: absolute;
+                        margin-top: -20px;
+                        margin-left: -10px;
+                    }
+                    .header .home_img
+                    {
+                        border: 1px solid black;
+                        height: 270px;
+                        width: 720px;
+                        position: absolute;
+                        margin-top: 140px;
+                        margin-left: 40px;
+                        padding: 0;
+                    }
+                    
+                    .header .home_img ul
+                    {
+                        margin: 0;
+                        padding: 0;
+                        
+                    }
+                    
+                    .header .home_img ul li
+                    {
+                        list-style: none;
+                        
+                    }
+
+                    .header .home_img #img1
+                    {
+                        height: 230px;
+                        width: 300px;
+                        position: absolute;
+                        padding-top: 20px;
+                    }
+
+                    .header .home_img table
+                    {
+                        margin: 0 0 0 285px;
+                        padding: 0;
+                    }
+                    .header .home_img table tr
+                    {
+                        margin: 0;
+                        padding: 0;
+                    }
+                    .header .home_img table tr td
+                    {
+                        margin: 0;
+                        padding: 0;
+                    }
+                    .header .home_img #img2
+                    {
+                        position: absolute;
+                        height: 130px;
+                        width: 160px;
+                        padding-top: 20px;
+                        margin-left: -10px;
+                    }
+                    .header .home_img #img3
+                    {
+                        position: absolute;
+                        height: 115px;
+                        width: 150px;
+                        padding-top: 130px;
+                        margin-left: -6px;
+                    }
+                    .header .home_img #img4
+                    {
+                        position: absolute;
+                        height: 115px;
+                        width: 150px;
+                        padding-top: 22px;
+                        padding-left: 140px;
+                    }
+                    .header .home_img #img5
+                    {
+                        position: absolute;
+                        height: 115px;
+                        width: 150px;
+                        padding-top: 130px;
+                        padding-left: 140px;
+                    }
+                </style>
+                <img src="image/logo.png"/>
+                <div class="home_img">
+                    <ul>
+                        <li>
+                            <img src="image/worktime.gif" id="img1"/>
+                        </li>
+                        <li>
+                            <table>
+                                <tr>
+                                    <td><img src="image/order.gif" id="img2"/></td>
+                                    <td><img src="image/condition.gif" id="img3"/></td>
+                                </tr>
+                                <tr>
+                                    <td><img src="image/shipping.gif" id="img4"/></td>
+                                    <td><img src="image/payment.gif" id="img5"/></td>
+                                </tr>
+                            </table>
+                        </li>
+                        <li>
+                            <ul>
+                                <li>Shinta</li>
+                                <li>Sarah</li>
+                                <li>Fanny</li>
+                                <li>Friska</li>
+                            </ul>
+                        </li>
+                    </ul>
+
+
+
+                </div>
             </div>
-        </div>
-        <div class="content">
-            content
-        </div>
-        <div class="footer">
-            footer
-        </div>
+            <div class="content">
+                content
+            </div>
+            <div class="footer">
+                footer
+            </div>
         </div> <!--end wrap-->
-        
+
     </body>
 </html>
