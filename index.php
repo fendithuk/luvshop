eccf4e389d05d8c331012cab2b07a9a01baa2a46
ngadiono <cristiano_ondoel@yahoo.com>
2011-12-27 18:03:37 +0700

    memulai rancangan

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title></title>
+        <link rel="stylesheet" type="text/css" href="css/style_utama.css"/>
+    </head>
+    <body>
+        <div class="wrap"> <!--start wrap-->
+        <div class="header">
+            header
+        </div>
+        <div class="content">
+            content
+        </div>
+        <div class="footer">
+            footer
+        </div>
+        </div> <!--end wrap-->
+        
+    </body>
+</html>
