94bb364841b873c6b5a38f62a2e2951da8d8a863
ngadiono <cristiano_ondoel@yahoo.com>
2011-12-27 19:09:19 +0700

    nambahin efek pink

--- index.php
+++ index.php
@@ -4,11 +4,41 @@
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
         <title></title>
         <link rel="stylesheet" type="text/css" href="css/style_utama.css"/>
+        <link rel="stylesheet" type="text/css" href="css/reset.css"/>
     </head>
     <body>
         <div class="wrap"> <!--start wrap-->
         <div class="header">
-            header
+            <style>
+                .header img
+                {
+                    height: 160px;
+                    width: 370px;
+                    position: absolute;
+                    margin-top: -20px;
+                    margin-left: -10px;
+                }
+                .header .home_img
+                {
+                    border: 1px solid black;
+                    height: 270px;
+                    width: 720px;
+                    position: absolute;
+                    margin-top: 140px;
+                    margin-left: 40px;
+                    padding: 0;
+                }
+                
+                .header .home_img #img1
+                {
+                    height: 200px;
+                    width: 200px;
+                }
+            </style>
+            <img src="image/logo.png"/>
+            <div class="home_img">
+                <img src="image/worktime.gif" id="img1"/>
+            </div>
         </div>
         <div class="content">
             content
