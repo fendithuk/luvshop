87636afd5a44574e2084b35a61480564df2d41a9
ngadiono <cristiano_ondoel@yahoo.com>
2011-12-29 19:35:57 +0700

    selesai membuat page order

--- home/order.php
+++ home/order.php
@@ -88,10 +88,7 @@
                         .content_right .submenu_right
                         {
                             border: 1px solid #cbcbcb;
-                            padding-top: 10px;
-                            padding-bottom: 40px;
-                            padding-left: 10px;
-                            padding-right: 10px;
+                            padding: 10px;
                             margin-top: -10px;
                             border-radius: 30px;
                             -webkit-border-radius: 30px;
@@ -103,7 +100,10 @@
                             margin: 0;
                             padding: 0;
                         }
-                        
+                        /*                        .content_right .submenu_right table tr
+                                                {
+                                                    margin-bottom: 15px;
+                                                }*/
                         .content_right .submenu_right ul
                         {
                             margin: 0;
@@ -129,87 +129,67 @@
                             font-size: 11px;
                         }
                     </style>
-                    <div class="submenu_right">
-                        <style type="text/css">
-                            .submenu_right ul
+                    <div class="submenu_right" style="padding: 15px">
+                        <style>
+                            .submenu_right img
                             {
-                                margin: 0;
-                                padding: 0;
+                                height: 362px;
+                                width: 552px;
                             }
-                            .submenu_right #img_utama
+                            .submenu_right .list_order
                             {
-                                height: 356px;
-                                width: 319px;
                                 position: absolute;
-                                margin-top: -306px;
-                                margin-left: -10px
-                            }
-                            .submenu_right table
-                            {
-                                margin: 0;
-                                padding: 0;
+                                margin-top: -302px;
+                                margin-left: 244px;
                                 
                             }
-                            .submenu_right table tr td .img_kedua
+                            .submenu_right .list_order p
                             {
-                                height: 72px;
-                                width: 79px;
-                                margin-right: 3px;
+                                font-size: 10px;
+                                color: #1f282d;
+                                font-family: sans-serif;
+                                text-align: left
+                            }
+                            .submenu_right .list_order li
+                            {
+                                font-size: 8px;
+                                list-style: decimal;
+                                font-family: sans-serif;
+                                color: #1f282d;
                             }
                         </style>
-                        <ul style="list-style: none">
-                            <li style="display: inline-block"><img src="<?php include('../elemen/url.php'); ?>image/home/tas5.gif" id="img_utama"/></li>
-                            <li style="display: inline-block;margin-left: 288px">
-                                <ul style="margin-top: 10px">
-                                    <li>
-                                        <table>
-                                            <tr>
-                                                <td><img src="<?php include('../elemen/url.php'); ?>image/stock/taskecil1.gif" class="img_kedua"/></td>
-                                                <td><img src="<?php include('../elemen/url.php'); ?>image/stock/taskecil2.gif" class="img_kedua"/></td>
-                                                <td><img src="<?php include('../elemen/url.php'); ?>image/stock/taskecil3.gif" class="img_kedua"/></td>
-                                            </tr>
-                                            <tr>
-                                                <td><img src="<?php include('../elemen/url.php'); ?>image/stock/taskecil4.gif" class="img_kedua"/></td>
-                                                <td><img src="<?php include('../elemen/url.php'); ?>image/stock/taskecil5.gif" class="img_kedua"/></td>
-                                                <td><img src="<?php include('../elemen/url.php'); ?>image/stock/taskecil6.gif" class="img_kedua"/></td>
-                                            </tr>
-                                        </table>
-                                    </li>
-                                    <li>
-                                        <ul style="margin-top: 70px; position: static">
-                                            <li><h2 style="color: #960316;margin-bottom: 10px;">Rp.95.000</h2></li>
-                                            <li>
-                                                <p style="font-weight: bold; display: inline-block">Colour:</p>
-                                                
-                                                <select style="width: 102px; background-color: white;border: 1px solid #bec6d3;display: inline-block; margin-left: 12px; margin-bottom: 10px;">
-                                                    <option>
-                                                        
-                                                    </option>
-                                                </select>
-                                            </li>
-                                            <li>
-                                                <p style="font-weight: bold; display: inline-block">Quantity:</p>
-                                                <input style="border: 1px solid #bec6d3; width: 50px;"/>
-                                            </li>
-                                            <li><img src="<?php include('../elemen/url.php'); ?>image/addcart.gif" style="height: 23px; width: 87px;position: absolute; margin-top: 11px;"/></li>
-                                            
-                                        </ul>
-                                    </li>
-                                </ul>
-                            </li>
-                            <li>
-                                <ul style="margin-left: 24px;margin-top: 40px;">
-                                    <li><p style="text-align: left; font-weight: bold">VENTAGO</p></li>
-                                    <li><p style="text-align: left; font-weight: bold">Berat 0.5 kg</p></li>
-                                    <li><p style="text-align: left; font-weight: bold">Panjang: 40cm</p></li>
-                                    <li><p style="text-align: left; font-weight: bold">Lebar : 9cm</p></li>
-                                    <li><p style="text-align: left; font-weight: bold">Tinggi : 33cm</p></li>
-                                    <li><p style="text-align: left; font-weight: bold">Panjang Tali : 30cm</p></li>
-                                    <li><p style="text-align: left; font-weight: bold">Bahan : Kulit Sintetis</p></li>
-                                    
-                                </ul>
-                            </li>
-                        </ul>
+                        <img src="<?php include('../elemen/url.php'); ?>image/home/girl1.gif" />
+                        <div class="list_order">
+                            <p>
+                                How to Order ?
+                            </p>
+                            <br/>
+                            <p>
+                                Buat Account Baru
+                            </p>
+                            <br/>
+                            <ol>
+                                <li>Klik tulisan "Sign Up!" dikanan atas, isi data, tekan tombol Register.</li>
+                                <li>Cek email kalian, cari email activation link dari LuvShop boutique.<br/>buka dan klik link yang ada, Kini kalian bisa login diwebsite kami. </li>
+                            </ol>
+                            <br/>
+                            <p>
+                                cara ORDER
+                            </p>
+                            <br/>
+                            <ol>
+                                <li>Login.</li>
+                                <li>Pilih barang yang ingin dipesan.</li>
+                                <li>Tekan tombol add to cart pada barang yang ingin dipesan.</li>
+                                <li>Jika sudah selesai memilih barang2 yang ingin dipesan, <br/> klik tulisan "show cart" pada kotak Your Cart</li>
+                                <li>Klik tulisan "Checkout".</li>
+                                <li>Isi data Shipping Information.</li>
+                                <li>Tekan tombol next.</li>
+                                <li>Cek kembali data orderan kalian, jika sudah betul,<br/> tekan tombol Confirm Order.</li>
+                                <li>Untuk melihat data orderan yang pernah kalian buat, <br/>cari menu My account yang ada didalam main menu.</li>
+                            </ol>
+                        </div>
+
                     </div>
                 </div>
             </div>
--- home/paymen.php
+++ home/paymen.php
@@ -88,10 +88,7 @@
                         .content_right .submenu_right
                         {
                             border: 1px solid #cbcbcb;
-                            padding-top: 10px;
-                            padding-bottom: 40px;
-                            padding-left: 10px;
-                            padding-right: 10px;
+                            padding: 10px;
                             margin-top: -10px;
                             border-radius: 30px;
                             -webkit-border-radius: 30px;
@@ -103,7 +100,10 @@
                             margin: 0;
                             padding: 0;
                         }
-                        
+                        /*                        .content_right .submenu_right table tr
+                                                {
+                                                    margin-bottom: 15px;
+                                                }*/
                         .content_right .submenu_right ul
                         {
                             margin: 0;
@@ -130,86 +130,7 @@
                         }
                     </style>
                     <div class="submenu_right">
-                        <style type="text/css">
-                            .submenu_right ul
-                            {
-                                margin: 0;
-                                padding: 0;
-                            }
-                            .submenu_right #img_utama
-                            {
-                                height: 356px;
-                                width: 319px;
-                                position: absolute;
-                                margin-top: -306px;
-                                margin-left: -10px
-                            }
-                            .submenu_right table
-                            {
-                                margin: 0;
-                                padding: 0;
-                                
-                            }
-                            .submenu_right table tr td .img_kedua
-                            {
-                                height: 72px;
-                                width: 79px;
-                                margin-right: 3px;
-                            }
-                        </style>
-                        <ul style="list-style: none">
-                            <li style="display: inline-block"><img src="<?php include('../elemen/url.php'); ?>image/home/tas5.gif" id="img_utama"/></li>
-                            <li style="display: inline-block;margin-left: 288px">
-                                <ul style="margin-top: 10px">
-                                    <li>
-                                        <table>
-                                            <tr>
-                                                <td><img src="<?php include('../elemen/url.php'); ?>image/stock/taskecil1.gif" class="img_kedua"/></td>
-                                                <td><img src="<?php include('../elemen/url.php'); ?>image/stock/taskecil2.gif" class="img_kedua"/></td>
-                                                <td><img src="<?php include('../elemen/url.php'); ?>image/stock/taskecil3.gif" class="img_kedua"/></td>
-                                            </tr>
-                                            <tr>
-                                                <td><img src="<?php include('../elemen/url.php'); ?>image/stock/taskecil4.gif" class="img_kedua"/></td>
-                                                <td><img src="<?php include('../elemen/url.php'); ?>image/stock/taskecil5.gif" class="img_kedua"/></td>
-                                                <td><img src="<?php include('../elemen/url.php'); ?>image/stock/taskecil6.gif" class="img_kedua"/></td>
-                                            </tr>
-                                        </table>
-                                    </li>
-                                    <li>
-                                        <ul style="margin-top: 70px; position: static">
-                                            <li><h2 style="color: #960316;margin-bottom: 10px;">Rp.95.000</h2></li>
-                                            <li>
-                                                <p style="font-weight: bold; display: inline-block">Colour:</p>
-                                                
-                                                <select style="width: 102px; background-color: white;border: 1px solid #bec6d3;display: inline-block; margin-left: 12px; margin-bottom: 10px;">
-                                                    <option>
-                                                        
-                                                    </option>
-                                                </select>
-                                            </li>
-                                            <li>
-                                                <p style="font-weight: bold; display: inline-block">Quantity:</p>
-                                                <input style="border: 1px solid #bec6d3; width: 50px;"/>
-                                            </li>
-                                            <li><img src="<?php include('../elemen/url.php'); ?>image/addcart.gif" style="height: 23px; width: 87px;position: absolute; margin-top: 11px;"/></li>
-                                            
-                                        </ul>
-                                    </li>
-                                </ul>
-                            </li>
-                            <li>
-                                <ul style="margin-left: 24px;margin-top: 40px;">
-                                    <li><p style="text-align: left; font-weight: bold">VENTAGO</p></li>
-                                    <li><p style="text-align: left; font-weight: bold">Berat 0.5 kg</p></li>
-                                    <li><p style="text-align: left; font-weight: bold">Panjang: 40cm</p></li>
-                                    <li><p style="text-align: left; font-weight: bold">Lebar : 9cm</p></li>
-                                    <li><p style="text-align: left; font-weight: bold">Tinggi : 33cm</p></li>
-                                    <li><p style="text-align: left; font-weight: bold">Panjang Tali : 30cm</p></li>
-                                    <li><p style="text-align: left; font-weight: bold">Bahan : Kulit Sintetis</p></li>
-                                    
-                                </ul>
-                            </li>
-                        </ul>
+                        
                     </div>
                 </div>
             </div>
