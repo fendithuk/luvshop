c8f237995cf57a92a61ed3be9c619683ae849a9b
ngadiono <cristiano_ondoel@yahoo.com>
2011-12-29 18:45:31 +0700

    selesai page member stock detail

--- memberarea/memberarea-detail-stock.php
+++ memberarea/memberarea-detail-stock.php
@@ -88,7 +88,10 @@
                         .content_right .submenu_right
                         {
                             border: 1px solid #cbcbcb;
-                            padding: 10px;
+                            padding-top: 10px;
+                            padding-bottom: 40px;
+                            padding-left: 10px;
+                            padding-right: 10px;
                             margin-top: -10px;
                             border-radius: 30px;
                             -webkit-border-radius: 30px;
@@ -135,8 +138,11 @@
                             }
                             .submenu_right #img_utama
                             {
-                                height: 341px;
+                                height: 356px;
                                 width: 319px;
+                                position: absolute;
+                                margin-top: -306px;
+                                margin-left: -10px
                             }
                             .submenu_right table
                             {
@@ -148,46 +154,58 @@
                             {
                                 height: 72px;
                                 width: 79px;
+                                margin-right: 3px;
                             }
                         </style>
                         <ul style="list-style: none">
                             <li style="display: inline-block"><img src="<?php include('../elemen/url.php'); ?>image/home/tas5.gif" id="img_utama"/></li>
-                            <li style="display: inline-block;margin-left: -30px">
-                                <ul>
+                            <li style="display: inline-block;margin-left: 288px">
+                                <ul style="margin-top: 10px">
                                     <li>
                                         <table>
                                             <tr>
-                                                <td><img src="<?php include('../elemen/url.php'); ?>image/home/tas5.gif" class="img_kedua"/></td>
-                                                <td><img src="<?php include('../elemen/url.php'); ?>image/home/tas5.gif" class="img_kedua"/></td>
-                                                <td><img src="<?php include('../elemen/url.php'); ?>image/home/tas5.gif" class="img_kedua"/></td>
+                                                <td><img src="<?php include('../elemen/url.php'); ?>image/stock/taskecil1.gif" class="img_kedua"/></td>
+                                                <td><img src="<?php include('../elemen/url.php'); ?>image/stock/taskecil2.gif" class="img_kedua"/></td>
+                                                <td><img src="<?php include('../elemen/url.php'); ?>image/stock/taskecil3.gif" class="img_kedua"/></td>
                                             </tr>
                                             <tr>
-                                                <td><img src="<?php include('../elemen/url.php'); ?>image/home/tas5.gif" class="img_kedua"/></td>
-                                                <td><img src="<?php include('../elemen/url.php'); ?>image/home/tas5.gif" class="img_kedua"/></td>
-                                                <td><img src="<?php include('../elemen/url.php'); ?>image/home/tas5.gif" class="img_kedua"/></td>
+                                                <td><img src="<?php include('../elemen/url.php'); ?>image/stock/taskecil4.gif" class="img_kedua"/></td>
+                                                <td><img src="<?php include('../elemen/url.php'); ?>image/stock/taskecil5.gif" class="img_kedua"/></td>
+                                                <td><img src="<?php include('../elemen/url.php'); ?>image/stock/taskecil6.gif" class="img_kedua"/></td>
                                             </tr>
                                         </table>
                                     </li>
                                     <li>
-                                        <ul>
-                                            <li><h2>Rp.95.000</h2></li>
-                                            <li><p>Colour</p></li>
-                                            <li><p>Quantity</p></li>
-                                            <li><img src="<?php include('../elemen/url.php'); ?>image/cart.gif"/></li>
+                                        <ul style="margin-top: 70px; position: static">
+                                            <li><h2 style="color: #960316;margin-bottom: 10px;">Rp.95.000</h2></li>
+                                            <li>
+                                                <p style="font-weight: bold; display: inline-block">Colour:</p>
+                                                
+                                                <select style="width: 102px; background-color: white;border: 1px solid #bec6d3;display: inline-block; margin-left: 12px; margin-bottom: 10px">
+                                                    <option>
+                                                        
+                                                    </option>
+                                                </select>
+                                            </li>
+                                            <li>
+                                                <p style="font-weight: bold; display: inline-block">Quantity:</p>
+                                                <input style="border: 1px solid #bec6d3; width: 50px;"/>
+                                            </li>
+                                            <li><img src="<?php include('../elemen/url.php'); ?>image/addcart.gif" style="height: 23px; width: 87px;position: absolute; margin-top: 11px;"/></li>
                                             
                                         </ul>
                                     </li>
                                 </ul>
                             </li>
                             <li>
-                                <ul>
-                                    <li><p>VENTAGO</p></li>
-                                    <li><p>Berat 0.5 kg</p></li>
-                                    <li><p>Panjang: 40cm</p></li>
-                                    <li><p>Lebar : 9cm</p></li>
-                                    <li><p>Tinggi : 33cm</p></li>
-                                    <li><p>Panjang Tali : 30cm</p></li>
-                                    <li><p>Bahan : Kulit Sintetis</p></li>
+                                <ul style="margin-left: 24px;margin-top: 40px;">
+                                    <li><p style="text-align: left; font-weight: bold">VENTAGO</p></li>
+                                    <li><p style="text-align: left; font-weight: bold">Berat 0.5 kg</p></li>
+                                    <li><p style="text-align: left; font-weight: bold">Panjang: 40cm</p></li>
+                                    <li><p style="text-align: left; font-weight: bold">Lebar : 9cm</p></li>
+                                    <li><p style="text-align: left; font-weight: bold">Tinggi : 33cm</p></li>
+                                    <li><p style="text-align: left; font-weight: bold">Panjang Tali : 30cm</p></li>
+                                    <li><p style="text-align: left; font-weight: bold">Bahan : Kulit Sintetis</p></li>
                                     
                                 </ul>
                             </li>
